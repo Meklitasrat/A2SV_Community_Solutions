ca86b948a9003e8f6b85bba45c4bce9b3d50925c
Meklit Asrat <148390664+Meklitasrat@users.noreply.github.com>
2024-06-27 00:10:53 +0300

    Create list_compr.py

---
+++ list_compr.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    Ans = []
+    for i in range(x+1):
+        for j in range(y +1):
+            for k in range(z +1):
+                if i + j +k != n:
+                
+                   Ans.append([i,j,k])
+
+    
+
+
+    print(Ans)
+  
+
+   
